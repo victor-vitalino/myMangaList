1373d9b45050b5f54a9e6de75e02ab6b3d319c48
Victor Vitalino <victorvitalino237@gmail.com>
2024-03-31 22:42:05 -0300

    feat: model and migration of mangas

---
+++ backend/app/models/manga.ts
@@ -0,0 +1,65 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, belongsTo, column } from '@adonisjs/lucid/orm'
+import { v4 } from 'uuid'
+import User from './user.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Category from './category.js'
+
+export default class Manga extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare slug: string
+
+  @column()
+  declare synopsis: string
+
+  @column()
+  declare stars: number
+
+  @column()
+  declare status: string
+
+  @column()
+  declare type: 'ANIME' | 'MANGA' | 'MANHUA' | 'COMIC' | 'NOVEL'
+
+  @column({ columnName: 'en_title' })
+  declare enTitle: string
+
+  @column({ columnName: 'jp_title' })
+  declare jpTitle: string
+
+  @column({ columnName: 'poster_img' })
+  declare posterImg: string
+
+  @column({ columnName: 'cover_img' })
+  declare coverImg: string
+
+  @column({ columnName: 'category_id' })
+  declare categoryId: string
+
+  @column({ columnName: 'user_id' })
+  declare userId: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @beforeCreate()
+  static createUUID(model: Manga) {
+    model.id = v4()
+  }
+
+  @belongsTo(() => User, {
+    foreignKey: 'userId',
+  })
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Category, {
+    foreignKey: 'categoryId',
+  })
+  declare category: BelongsTo<typeof Category>
+}
---
+++ backend/database/migrations/1711932722273_create_manga_table.ts
@@ -0,0 +1,29 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'manga'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('en_title')
+      table.string('jp_title')
+      table.string('slug')
+      table.string('synopsis')
+      table.double('stars').defaultTo(0)
+      table.string('status')
+      table.enum('type', ['ANIME', 'MANGA', 'MANHUA', 'COMIC', 'NOVEL'])
+      table.string('poster_img')
+      table.string('cover_img')
+      table.integer('category_id').references('id').inTable('categories')
+      table.integer('user_id').references('id').inTable('users')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
